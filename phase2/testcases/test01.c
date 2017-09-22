7b15f1ecbcb952adaf22ffef6e2c5bb14317f457
Shaion Moghimi <shaionmoghimi@gmail.com>
2017-09-22 10:01:51 -0700

    Initial setup for phase2.

---
+++ phase2/testcases/test01.c
@@ -0,0 +1,29 @@
+
+/* start2 creates two mailboxes, then quits
+ */
+
+#include <stdio.h>
+#include <usloss.h>
+#include <phase1.h>
+#include <phase2.h>
+
+void test_setup(int argc, char *argv[])
+{
+}
+
+void test_cleanup(int argc, char *argv[])
+{
+}
+
+int start2(char *arg)
+{
+  int mbox_id;
+
+  printf("start2(): started\n");
+  mbox_id = MboxCreate(10, 50);
+  printf("start2(): MailBoxCreate returned id = %d\n", mbox_id);
+  mbox_id = MboxCreate(20, 30);
+  printf("start2(): MailBoxCreate returned id = %d\n", mbox_id);
+  quit(0);
+  return 0; /* so gcc will not complain about its absence... */
+}
